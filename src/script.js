984cfe0508dfc01ca4a29b35e5daa1616f46c5f8
Mandy Gutierrez <120603830+Mandyguti20@users.noreply.github.com>
2023-06-16 15:42:29 -0400

    Added current time element

--- src/script.js
+++ src/script.js
@@ -9,14 +9,23 @@ losAngelesDate.innerHTML = losAngelesTimeFormat.format("dddd, MMMM Do, YYYY");
 losAngelesTime.innerHTML = `${losAngelesTimeFormat.format("h:mm:ss")}
 <small>${losAngelesTimeFormat.format("a")}</small>`;
 
-//New York Time Element
-let  = document.querySelector("#tokyo");
+//Tokyo Time Element
+let tokyo = document.querySelector("#tokyo");
 let tokyoDate = tokyo.querySelector(".city-date");
 let tokyoTime = tokyo.querySelector(".city-time");
 let tokyoTimeFormat = moment().tz(`Asia/Tokyo`);
 tokyoDate.innerHTML = tokyoTimeFormat.format("dddd, MMMM Do, YYYY");
 tokyoTime.innerHTML = `${tokyoTimeFormat.format("h:mm:ss")}
 <small>${tokyoTimeFormat.format("a")}</small>`;
+
+//Current Time Element
+let currentTime  = document.querySelector(".current-time");
+let currentDate = document.querySelector(".current-date");
+let currentTimeFormat = moment().tz(`America/New_York`);
+let currentAmPmElement = document.querySelector("#am-pm");
+currentDate.innerHTML = currentTimeFormat.format("dddd, MMMM Do, YYYY");
+currentTime.innerHTML = `${currentTimeFormat.format("h:mm")}`;
+currentAmPmElement.innerHTML = currentTimeFormat.format("a");
 
 }
 
